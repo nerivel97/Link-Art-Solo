f6f3766c4df956544e450257624de2d2c57c18b8
Alan Llanes <llanesalan31@gmail.com>
2024-10-21 23:52:18 -0500

    Actualizacion

---
+++ src/controllers/entries.controller.js
@@ -0,0 +1,45 @@
+/*
+Para añadir una funcion de renderizado de por ejemplo usuarios.ejs, copiaremos las lineas cambiando el nombre
+const renderUsuario = (req, res) => {
+    res.render('index');
+después añadimos "renderUsuario," en la parte de module.exports
+
+};
+
+*/
+
+/*  añadan ,{ currentPage: 'nombrePagina' } */
+
+const renderIndex = (req, res) => {
+    res.render('index', { currentPage: 'index' });
+};
+
+const renderLogin = (req, res) => {
+    res.render('login', { currentPage: 'login' });
+};
+
+const renderComprar = (req, res) => {
+    res.render('comprar', { currentPage: 'comprar' });
+};
+
+const renderArtistas = (req, res) => {
+    res.render('artistas', { currentPage: 'artistas' });
+};
+
+const renderNewEntry = (req, res) => {};
+
+const createNewEntry = (req, res) => {};
+
+const renderRegister = (req, res) => {
+    res.render('register');
+};
+
+module.exports = {
+    renderIndex,
+    renderNewEntry,
+    createNewEntry,
+    renderLogin,
+    renderComprar,
+    renderArtistas,
+    renderRegister
+};
